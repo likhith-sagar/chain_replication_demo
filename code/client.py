3099163a3d7754b1e31e65183bc5edcc378e016f
Likhith <likhith.likhithsagar@outlook.com>
2022-11-03 11:54:04 +0530

    client idea changes

--- code/client.py
+++ code/client.py
@@ -1,4 +1,7 @@
 '''
-Client to be implemented using Flask to provided
-generalized HTTP based APIs
+Client might have to be implemented using rpyc only for now
+but should see how to use flask to provide generalised apis to user
+may be using one more program (running on top of client) or together
+(Its getting complicated because if we have to respond to user if an update req is successful or not,
+if we don't have to respond, then its simple)
 '''
